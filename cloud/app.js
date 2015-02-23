407c809185c9f68df3c4942fbf1b9f7a1f8b393c
sccalex <ruixinhan@Ruixins-MacBook-Pro.local>
2015-02-23 21:33:32 +1100

    V0.01
    Added landing page.
    
    #27538742426745

--- cloud/app.js
+++ cloud/app.js
@@ -9,7 +9,11 @@ app.use(express.bodyParser());    // 读取请求 body 的中间件
 
 // 使用 Express 路由 API 服务 /hello 的 HTTP GET 请求
 app.get('/hello', function(req, res) {
-  res.render('hello', { message: 'Congrats, you just set up your app!' });
+	//Do something, e.g. get data
+	var downloadedTitle = req.query.headline;
+	var getMenu = req.query.pageMenu;
+	//Display page
+  	res.render('hello', { pageTitle: downloadedTitle, pageMenu: getMenu });
 });
 
 // 最后，必须有这行代码来使 express 响应 HTTP 请求
