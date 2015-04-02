f34a6679483e14453bdcf10c9b62613c8594d8be
crucified713 <tianyili0713@gmail.com>
2015-04-03 00:42:26 +1100

    v0.24
    * Removed the logo
    * Added contents for <Our Service>
    * Changed title to “豪華遊艇會”
    * Fixed the layout

--- cloud/public/js/directives.js
+++ cloud/public/js/directives.js
@@ -73,3 +73,14 @@ JMSApp.directive('ngReallyClick', [
 		}
 	}
 ]);
+
+JMSApp.directive('Collage', function(){
+	return {
+		restrict: 'A',
+		link: function(scope, element, attrs) {
+			alert("Collage worked");
+			element.removeWhitespace().collagePlus();
+
+		}
+	}
+})
